0d9888d4b9a0b1d4121093b341b2c31cfe798e2f
HallWood <654663951@qq.com>
2018-09-26 10:51:09 +0800

    fix the def err

--- header/graph/graph_def.h
+++ header/graph/graph_def.h
@@ -5,28 +5,39 @@
 #define MAXVEXNUM 255
 #define MAXINTVAL (~(unsigned int)0 >> 1)
 
+// forward def
+struct Graph;
+
+template <typename T> class MatVex;
+template <typename T> class MatGraph;
+
+template <typename T> class ArcNode;
+template <typename T> class ArcVex;
+template <typename T> class ArcGraph;
+
+
 struct Graph {
 public:
     int vexNum;
     int edgeNum;
 
-    Graph(int v, int e) vexNum(v), edgeNum(e) {}
-}
+    Graph(int v, int e): vexNum(v), edgeNum(e) {}
+};
 
 template <typename T>
 class MatVex {
 public:
-    friend class MatGraph;
+    friend class MatGraph<T>;
     T data;
-}
+};
 
 template <typename T>
-class MatGraph {
+class MatGraph: public Graph {
 public:
     int edges[MAXVEXNUM][MAXVEXNUM];
-    MatVex<T> vexList;     
+    MatVex<T> vexList[MAXVEXNUM];     
 
-    MatGraph(int v = 0, int e = 0, const MaxVex<T>* vl, int* edgs[] = nullptr): Graph(v, e) {
+    MatGraph(int v = 0, int e = 0, MatVex<T>* vl = nullptr, int* edgs[] = nullptr): Graph(v, e) {
         if(edgs) {
             for(int i = 0; i < MAXVEXNUM; ++i) {
 		        for(int j = 0; j < MAXVEXNUM; ++j) {
@@ -35,37 +46,38 @@ public:
 	        }
         } else for(int i = 0; i < MAXVEXNUM; ++i) memset(edges[i], MAXINTVAL, MAXVEXNUM*sizeof(int));
         
-	if(vl) for(int i = 0; i < MAXVEXNUM; ++i) vexList[i] = vl[i];
+	    if(vl) for(int i = 0; i < MAXVEXNUM; ++i) vexList[i] = vl[i];
         else memset(vexList, T(), sizeof(vexList));
     }
-}
+};
 
 template <typename T>
 class ArcVex {
 public:
-    friend class ArcGraph;
+    friend class ArcGraph<T>;
     T data;
-    ArcNode* firstArcPtr;
-}
+    ArcNode<T>* firstArcPtr;
+};
 
 
 template <typename T>
-class ArcGraph {
+class ArcGraph: public Graph {
 public:
     ArcVex<T> adjlist[MAXVEXNUM];
 
-    ArcGraph(int v  = 0, int e = 0. ArcVex* al = nullptr): Graph(v, e) {
+    ArcGraph(int v  = 0, int e = 0, ArcVex<T>* al = nullptr): Graph(v, e) {
 	    if(al) {
 	        for(int i = 0; i < MAXVEXNUM; ++i)
 		        adjlist[i] = al[i]; 
         }
     }
-}
+};
 
+template<typename T>
 class ArcNode {
 public:
-    friend class ArcGraph;
+    friend class ArcGraph<T>;
     int adjVexId;
     
-    ArcNode* next;
-}
+    ArcNode<T>* next;
+};
