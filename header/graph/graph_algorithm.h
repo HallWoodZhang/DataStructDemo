0c82a1834fd9f1b6e5d998d28fa10e60e98d138c
HallWood <654663951@qq.com>
2018-09-27 03:08:32 +0800

    add a top sort algorithm demo

--- header/graph/graph_algorithm.h
+++ header/graph/graph_algorithm.h
@@ -4,12 +4,15 @@
 #include "graph_def.h"
 
 template <typename T>
-int* Dijkstra(MatGraph<T>& G, int startVexId);
+int* Dijkstra(MatGraph<T>& G, int startVexId = 0);
 
 template <typename T>
 int Kruskal(MatGraph<T>* G, Road* roads = nullptr);
 
 template <typename T>
 int Prim(MatGraph<T>* G, int startVexId = 0);
+
+template <typename T>
+bool TopSort(const ArcGraph<T>& G, int indegree[MAXVEXNUM]);
 
 #endif
---
+++ src/graph/TopSort.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+#include "graph/graph_def.h"
+#include "graph/graph_algorithm.h"
+
+using namespace std;
+
+// indegree: the array about indegree of vertex
+// return: if the function works well
+template <typename T>
+bool TopSort(const ArcGraph<T>& G, int indegree[MAXVEXNUM]) {
+    int stack[MAXVEXNUM], top = -1;
+    int counter = 0;
+    for(int i = 0; i < G.vexNum; ++i) {
+        if(indegree[i] == 0)
+            stack[++top] = i;
+    }
+
+    while(top != -1) {
+        int tmpVexId = stack[top--];
+        ++counter;
+        cout << tmpVexId << ' ';
+
+        ArcNode* tmpPtr = G.adjlist[tmpVexId].firstArcPtr;
+        while(tmpPtr) {
+            int nextVexId = tmpPtr->adjVexId;
+            indegree[nextVexId] -= 1;
+            if(indegree[nextVexId] == 0) stack[++top] = nextVexId;
+            tmpPtr = tmpPtr->next;
+        }
+    }
+
+    return counter == G.vexNum;
+}
