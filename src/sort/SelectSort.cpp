4d46c47abcc5a1883c5419bf4d0fb4e0f3964cda
HallWood <654663951@qq.com>
2018-09-27 10:17:28 +0800

    add a select sort algorithm demo

---
+++ src/sort/SelectSort.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+#include "sort/sort_algorithm.h"
+
+using namespace std;
+
+template <typename T>
+static inline void swap(T& lhs, T& rhs) {
+    T tmp = lhs;
+    lhs = rhs;
+    rhs = tmp;
+}
+
+template <typename T>
+void SelectSort(T* beg, T* end) {
+    int size = end - beg;
+    for(int i = 0; i < size; ++i) {
+        int k = i;
+        for(int j = i + 1; j < size; ++j) {
+            if(beg[k] > beg[j])
+                k = j;
+        }
+        swap(beg[i], beg[k]);
+    }
+}
